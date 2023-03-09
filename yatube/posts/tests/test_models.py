1cf9e9b876c589e54ee486b58348be1c52f29975
Dmitrii Ounap <ounap2016@yandex.ru>
2023-03-09 19:27:55 +0500

    committed4

---
+++ yatube/posts/tests/test_models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from ..models import Group, Post
+
+User = get_user_model()
+
+
+class PostModelTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='auth')
+        cls.group = Group.objects.create(
+            title='Тестовая группа',
+            slug='Тестовый слаг',
+            description='Тестовое описание',
+        )
+        cls.post = Post.objects.create(
+            author=cls.user,
+            text='Тестовый пост',
+        )
+
+    def test_models_have_correct_object_names(self):
+        text = PostModelTest.post
+        expected_text = text.title
+        group = PostModelTest.group
+        expected_group = group.title
+        self.assertEqual(expected_text, str(text))
+        self.assertEqual(expected_group, str(group))
